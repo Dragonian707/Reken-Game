187e4bb05a317c4510e62fd05fe7afde76376383
RTVHypeGamer <114234122+RTVHypeGamer@users.noreply.github.com>
2024-06-03 13:46:50 +0200

    mathgenrator minigame 1

---
+++ Assets/minigame 1 assets/Scripts/MathGenerator.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using TMPro;
+
+public class MathGenerator : MonoBehaviour
+{
+    int FirstRandomnumber;
+    int SecondRandomnumber;
+    [SerializeField] TMP_Text displayNumber1;
+    [SerializeField] TMP_Text displayNumber2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            CreateRandomNumber();
+        }
+    }
+
+    public void CreateRandomNumber()
+    {
+        FirstRandomnumber = Random.Range(1, 201);
+        SecondRandomnumber = Random.Range(1, 201);
+        displayNumber1.text = FirstRandomnumber.ToString();
+        displayNumber2.text = SecondRandomnumber.ToString();
+    }
+}
