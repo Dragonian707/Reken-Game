aeca9d71deb88b857c5d8ab8f3e0e628838dda63
LegacyOfTheArdor <95230814+LegacyOfTheArdor@users.noreply.github.com>
2024-06-17 14:01:21 +0200

    fix knob turning

--- Assets/kluis_minigame_Assets/scripts/TurnKnob.cs
+++ Assets/kluis_minigame_Assets/scripts/TurnKnob.cs
@@ -19,28 +19,33 @@ public class TurnKnob : MonoBehaviour
 
 	// Update is called once per frame
 	void Update()
-    { 
+    {
+		RaycastHit2D ray = Physics2D.Raycast(Input.mousePosition, Vector3.forward);
 
-		Vector3 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
-		if (Input.GetMouseButtonDown(0))
+		if(ray.collider != null) 
 		{
-			if (turner == Physics2D.OverlapPoint(mousePos))
+			Vector3 mousePos = _camera.ScreenToWorldPoint(Input.mousePosition);
+			if (Input.GetMouseButtonDown(0))
 			{
-				screenpos = _camera.ScreenToWorldPoint(transform.position);
-				Vector3 vec3 = Input.mousePosition - screenpos;
-				angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;
+				if (turner == Physics2D.OverlapPoint(mousePos))
+				{
+					screenpos = _camera.ScreenToWorldPoint(transform.position);
+					Vector3 vec3 = Input.mousePosition - screenpos;
+					angleOffset = (Mathf.Atan2(transform.right.y, transform.right.x) - Mathf.Atan2(vec3.y, vec3.x)) * Mathf.Rad2Deg;
 
+				}
+			}
+			if (Input.GetMouseButton(0))
+			{
+				if (turner == Physics2D.OverlapPoint(mousePos))
+				{
+					Vector3 vec3 = new Vector3(Input.mousePosition.x - Screen.currentResolution.width / 2, Input.mousePosition.y - Screen.currentResolution.height * 0.22222f , 0);
+					float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
+					transform.eulerAngles = new Vector3(0, 0, angle + angleOffset);
+
+				}
 			}
 		}
-		if (Input.GetMouseButton(0))
-		{
-			if (turner == Physics2D.OverlapPoint(mousePos))
-			{
-				Vector3 vec3 = Input.mousePosition - screenpos;
-				float angle = Mathf.Atan2(vec3.y, vec3.x) * Mathf.Rad2Deg;
-				transform.eulerAngles = new Vector3(0, 0, angle + angleOffset);
-
-			}
-		}
+		
 	}
 }
