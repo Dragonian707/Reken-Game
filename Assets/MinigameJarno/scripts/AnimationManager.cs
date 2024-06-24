6ff714b8c7f755a5765efcd1ee123d19bdac28e8
Dragonian707 <jarnodijk2001@hotmail.com>
2024-06-24 13:37:39 +0200

    added home button to darts

--- Assets/MinigameJarno/scripts/AnimationManager.cs
+++ Assets/MinigameJarno/scripts/AnimationManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class AnimationManager : MonoBehaviour
 {
@@ -8,5 +9,10 @@ public class AnimationManager : MonoBehaviour
     public void ShowExplanation()
     {
         explainAnim.SetBool("exp", !explainAnim.GetBool("exp"));
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
     }
 }
